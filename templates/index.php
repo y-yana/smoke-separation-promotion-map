febecf54d1f25a111f2be1064df5faa0a9dd3bb0
y-yana <66319933+y-yana@users.noreply.github.com>
2020-12-03 03:28:21 +0900

    微修正

--- templates/index.php
+++ templates/index.php
@@ -1,73 +1,73 @@
 <?php $title = 'TopPage'; ?>
 <?php include('./header.php'); ?>
 
-<!-- 検索エリア -->
 <form>
   <div>
     <span>
-      <label><input type="checkbox" name="roof">屋根</label>
+      <label><input type="checkbox" name="checkBox" onChange="formCheck()">屋根</label>
     </span>
     <span>
-      <label><input type="radio" name="radioRoof" value="0" disabled>あり</label>
-      <label><input type="radio" name="radioRoof" value="1" disabled>なし</label>
-    </span>
-  </div>
-  <div>
-    <span>
-      <label><input type="checkbox" name="bench">ベンチ</label>
-    </span>
-    <span>
-      <label><input type="radio" name="radioBench" value="0" disabled>あり</label>
-      <label><input type="radio" name="radioBench" value="1" disabled>なし</label>
+      <label><input type="radio" name="roof" id="radioBtn10" value="0" disabled>あり</label>
+      <label><input type="radio" name="roof" id="radioBtn20" value="1" disabled>なし</label>
     </span>
   </div>
   <div>
     <span>
-      <label><input type="checkbox" name="enclosure">囲い</label>
+      <label><input type="checkbox" name="checkBox" onChange="formCheck()">ベンチ</label>
     </span>
     <span>
-      <label><input type="radio" name="radioEnclosure" value="0" disabled>あり</label>
-      <label><input type="radio" name="radioEnclosure" value="1" disabled>なし</label>
-    </span>
-  </div>
-  <div>
-    <span>
-      <label><input type="checkbox" name="cigaretteVendingMachine">タバコの自販機</label>
-    </span>
-    <span>
-      <label><input type="radio" name="radioCigaretteVendingMachine" value="0" disabled>あり</label>
-      <label><input type="radio" name="radioCigaretteVendingMachine" value="1" disabled>なし</label>
+      <label><input type="radio" name="bench" id="radioBtn11" value="0" disabled>あり</label>
+      <label><input type="radio" name="bench" id="radioBtn21" value="1" disabled>なし</label>
     </span>
   </div>
   <div>
     <span>
-      <label><input type="checkbox" name="vendingMachine">飲料の自販機</label>
+      <label><input type="checkbox" name="checkBox" onChange="formCheck()">囲い</label>
     </span>
     <span>
-      <label><input type="radio" name="radioVendingMachine" value="0" disabled>あり</label>
-      <label><input type="radio" name="radioVendingMachine" value="1" disabled>なし</label>
-    </span>
-  </div>
-  <div>
-    <span>
-      <label><input type="checkbox" name="electronicCigarette">利用できるタバコ</label>
-    </span>
-    <span>
-      <label><input type="radio" name="radioElectronicCigarette" value="0" disabled>全て</label>
-      <label><input type="radio" name="radioElectronicCigarette" value="1" disabled>電子タバコのみ</label>
+      <label><input type="radio" name="enclosure" id="radioBtn12" value="0" disabled>あり</label>
+      <label><input type="radio" name="enclosure" id="radioBtn22" value="1" disabled>なし</label>
     </span>
   </div>
   <div>
     <span>
-      <label><input type="checkbox" name="indoors">設置場所</label>
+      <label><input type="checkbox" name="checkBox" onChange="formCheck()">タバコの自販機</label>
     </span>
     <span>
-      <label><input type="radio" name="radioIndoors" value="0" disabled>屋外</label>
-      <label><input type="radio" name="radioIndoors" value="1" disabled>屋内</label>
+      <label><input type="radio" name="cigaretteVendingMachine" id="radioBtn13" value="0" disabled>あり</label>
+      <label><input type="radio" name="cigaretteVendingMachine" id="radioBtn23" value="1" disabled>なし</label>
+    </span>
+  </div>
+  <div>
+    <span>
+      <label><input type="checkbox" name="checkBox" onChange="formCheck()">飲料の自販機</label>
+    </span>
+    <span>
+      <label><input type="radio" name="vendingMachine" id="radioBtn14" value="0" disabled>あり</label>
+      <label><input type="radio" name="vendingMachine" id="radioBtn24" value="1" disabled>なし</label>
+    </span>
+  </div>
+  <div>
+    <span>
+      <label><input type="checkbox" name="checkBox" onChange="formCheck()">利用できるタバコ</label>
+    </span>
+    <span>
+      <label><input type="radio" name="electronicCigarette" id="radioBtn15" value="0" disabled>全て</label>
+      <label><input type="radio" name="electronicCigarette" id="radioBtn25" value="1" disabled>電子タバコのみ</label>
+    </span>
+  </div>
+  <div>
+    <span>
+      <label><input type="checkbox" name="checkBox" onChange="formCheck()">設置場所</label>
+    </span>
+    <span>
+      <label><input type="radio" name="indoors" id="radioBtn16" value="0" disabled>屋外</label>
+      <label><input type="radio" name="indoors" id="radioBtn26" value="1" disabled>屋内</label>
     </span>
   </div>
 </form>
 
 <script src="../static/js/GoogleMap.js"></script>
+<script src="../static/js/formCheck.js"></script>
 
 <?php include('./footer.php'); ?>
