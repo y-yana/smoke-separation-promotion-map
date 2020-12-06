1aca76e849a06c29f0c4b60169438a71817a8aaf
y-yana <66319933+y-yana@users.noreply.github.com>
2020-12-06 18:59:59 +0900

    個人ページへのリンク追加

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,8 @@
     <footer>
-      <a href="https://buturi.heteml.net/webPractice/2020/">2020年度</a>
+      <a href="https://buturi.heteml.net/webPractice/2020/">2020年度メディア演習Ⅱ</a>
+      <a href="https://buturi.heteml.net/webPractice/2020/18007">土肥美玲</a>
+      <a href="https://buturi.heteml.net/webPractice/2020/18052">延味優佳</a>
+      <a href="https://buturi.heteml.net/webPractice/2020/18060">柳田祐奈</a>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   </body>
