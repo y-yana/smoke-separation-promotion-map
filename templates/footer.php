5a4156ac6e4c81531fc5cfdefba80d3b7e3ad2a2
y-yana <66319933+y-yana@users.noreply.github.com>
2020-12-10 13:29:21 +0900

    footerレイアウト調整

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,12 @@
     <footer>
-      <a href="https://buturi.heteml.net/webPractice/2020/">2020年度メディア演習Ⅱ</a>
-      <a href="https://buturi.heteml.net/webPractice/2020/18007">土肥美玲</a>
-      <a href="https://buturi.heteml.net/webPractice/2020/18052">延味優佳</a>
-      <a href="https://buturi.heteml.net/webPractice/2020/18060">柳田祐奈</a>
+      <span class="footerClass">
+        <a href="https://buturi.heteml.net/webPractice/2020/">2020年度メディア演習Ⅱ</a>
+        <span class="footerName">
+          <a href="https://buturi.heteml.net/webPractice/2020/18007">土肥美玲</a>
+          <a href="https://buturi.heteml.net/webPractice/2020/18052">延味優佳</a>
+          <a href="https://buturi.heteml.net/webPractice/2020/18060">柳田祐奈</a>
+        </span>
+      </span>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   </body>
