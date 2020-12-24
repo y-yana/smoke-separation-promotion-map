3e0aea12ca94f43050127cfbe8c01ff6ca701ce4
y-yana <66319933+y-yana@users.noreply.github.com>
2020-12-24 14:20:59 +0900

    table作成

--- DataBase/createTable.php
+++ DataBase/createTable.php
@@ -1,11 +1,24 @@
   <?php
 
   // DB接続
-  require('../templates/env');
+  require('../templates/env.php');
 
   // sql文を格納
   $sql='CREATE TABLE smokingMap(
-    
+    ID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    placeName VARCHAR(30),
+    lat DOUBLE(9,6),
+    lng DOUBLE(9,6),
+    startTime VARCHAR(4),
+    endTime VARCHAR(4),
+    roof ENUM("Y","N"),
+    bench ENUM("Y","N"),
+    enclosure ENUM("Y","N"),
+    cigaretteSale ENUM("Y","N"),
+    drinkSale ENUM("Y","N"),
+    cigarette ENUM("all","electronic"),
+    outdoor ENUM("outdoor","indoor"),
+    message VARCHAR(500)
   )';
 
   // sql文を実行
