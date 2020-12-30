70580263b127f1dab3756bcc66c5a7f487b1f023
y-yana <66319933+y-yana@users.noreply.github.com>
2020-12-31 01:36:38 +0900

    改行

--- DataBase/insertTestData.php
+++ DataBase/insertTestData.php
@@ -5,10 +5,34 @@
 
   // sql文を格納
   $sql='INSERT INTO smokingMap(
-    placeName,lat,lng,startTime,endTime,roof,bench,enclosure,cigaretteSale,drinkSale,cigarette,outdoor,message
+    placeName,
+    lat,
+    lng,
+    startTime,
+    endTime,
+    roof,
+    bench,
+    enclosure,
+    cigaretteSale,
+    drinkSale,
+    cigarette,
+    outdoor,
+    message
     )
     VALUES(
-    "セブンイレブン東広島高美が丘店",34.451666993628685,132.78682775507812,"0000","2359","Y","N","N","Y","Y","all","outdoor","駐車場が広くて便利です。"
+    "セブンイレブン東広島高美が丘店",
+    34.451666993628685,
+    132.78682775507812,
+    "0000",
+    "2359",
+    "Y",
+    "N",
+    "N",
+    "Y",
+    "Y",
+    "all",
+    "outdoor",
+    "駐車場が広くて便利です。"
   )';
 
   // sql文を実行
