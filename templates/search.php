704a620368ef0bcd8bb3841f670df1da2326d685
y-yana <66319933+y-yana@users.noreply.github.com>
2021-01-01 23:14:20 +0900

    セッション追加

--- templates/search.php
+++ templates/search.php
@@ -1,5 +1,13 @@
 <?php
-  echo 'SELECT * FROM smokingMap WHERE
+
+  // DB接続
+  require('env.php');
+
+  // セッション開始
+  session_start();
+
+  // sql文を格納
+  $sql='SELECT * FROM smokingMap WHERE
   placeName LIKE "%'.$_POST['placeName'].'%" AND
   roof='.$_POST['roof'].' AND
   bench='.$_POST['bench'].' AND
@@ -8,4 +16,26 @@
   drinkSale='.$_POST['drinkSale'].' AND
   cigarette='.$_POST['cigarette'].' AND
   outdoor='.$_POST['outdoor'];
+
+  // sql文を実行
+  $results=$mysqli->query($sql);
+
+  foreach ($results as $row){
+    // 各情報をセッション変数に格納する
+    $_SESSION['placeName'] = $row['placeName'];
+    $_SESSION['lat'] = $row['lat'];
+    $_SESSION['lng'] = $row['lng'];
+    $_SESSION['roof'] = $row['roof'];
+    $_SESSION['bench'] = $row['bench'];
+    $_SESSION['enclosure'] = $row['enclosure'];
+    $_SESSION['cigaretteSale'] = $row['cigaretteSale'];
+    $_SESSION['drinkSale'] = $row['drinkSale'];
+    $_SESSION['cigarette'] = $row['cigarette'];
+    $_SESSION['outdoor'] = $row['outdoor'];
+    $_SESSION['message'] = $row['message'];
+  }
+
+  // ページ遷移
+  header("location: result.php");
+
 ?>
