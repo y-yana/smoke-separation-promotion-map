ed31387d6d1c1a813d42b2d50ee27fd3432566d6
y-yana <66319933+y-yana@users.noreply.github.com>
2021-01-05 19:29:12 +0900

    jQuery読み込み位置修正

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,5 @@
+    <script src="../static/js/modal.js"></script>
+
     <footer>
       <span class="footerClass">
         <a href="https://buturi.heteml.net/webPractice/2020/">2020年度メディア演習Ⅱ</a>
@@ -8,6 +10,5 @@
         </span>
       </span>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   </body>
   </html>
