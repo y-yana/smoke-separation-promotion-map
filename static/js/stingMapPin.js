2ed5450777654d18e46cce3638abbe7ab6bc9e48
y-yana <66319933+y-yana@users.noreply.github.com>
2021-01-13 20:54:33 +0900

    検索結果の場所にピンを刺した

--- static/js/stingMapPin.js
+++ static/js/stingMapPin.js
@@ -1,6 +1,23 @@
 function stingMapPin(id) {
+  // 検索結果の緯度経度を変数に代入
   var lat = document.getElementById("mapPin" + id).dataset.lat;
   var lng = document.getElementById("mapPin" + id).dataset.lng;
-  console.log(lat);
-  console.log(lng);
+
+  // mapの表示位置を指定
+  var result = new google.maps.LatLng(lat, lng);
+
+  // map表示
+  var myMap = new google.maps.Map(document.getElementById('myMap'), {
+    center: result,
+    zoom: 17,
+    mapTypeId: 'hybrid',
+    scrollwheel: false
+  });
+
+  // マーカーを表示
+  var marker = new google.maps.Marker({
+    position: result,
+    draggable: false
+  });
+  marker.setMap(myMap);
 }
